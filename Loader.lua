5a4220a0d029c88066f831b42a91f1acc327a21d
TAIDEN-garbage <113226115+TAIDEN-garbage@users.noreply.github.com>
2022-09-18 19:25:49 +0700

    Update and rename Loader.lua to Taidendit.luarin

--- Loader.lua
+++
@@ -1,9 +0,0 @@
-print("TAIDEN HUB NO.1")
-
-repeat wait() until game:IsLoaded()
-local PlaceId = game.PlaceId
-if PlaceId == 6516141723 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/bhpFlBHMKHBPPOLKMLOFFBhMkHKbLOFobKPkMml/z-d-z-z-z-a-locplayer/%E0%B8%95%E0%B9%8A%E0%B8%B8%E0%B8%97%E0%B8%B4%E0%B8%871%E0%B8%95%E0%B9%8A%E0%B8%B8%E0%B8%97%E0%B8%B4%E0%B8%872/door.lua")()
-elseif PlaceId == 2202352383 then
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/bhpFlBHMKHBPPOLKMLOFFBhMkHKbLOFobKPkMml/z-d-z-z-z-a-locplayer/%E0%B8%95%E0%B9%8A%E0%B8%B8%E0%B8%97%E0%B8%B4%E0%B8%871%E0%B8%95%E0%B9%8A%E0%B8%B8%E0%B8%97%E0%B8%B4%E0%B8%872/SPTSClassic.lua")()
-end
